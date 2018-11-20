44cdfe374d49ddffa825faef0487b10ca50c2d1e
acr0matic <herculesprod1@gmail.com>
2018-11-20 22:21:05 +0300

    Upload project in repository

---
+++ NumberKiller/NumberKiller/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace NumberKiller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(273, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(129, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Roboto Th", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(138, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 44);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ПУСК";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto Th", 10F);
+            this.label1.Location = new System.Drawing.Point(273, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "НОМЕР ТЕЛЕФОНА";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Roboto Th", 10F);
+            this.label2.Location = new System.Drawing.Point(18, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ИМЯ ЖЕРТВЫ";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(3, 84);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(396, 109);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 205);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ NumberKiller/NumberKiller/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberKiller
+{
+    public partial class Form1 : Form
+    {
+        private String number = "+79653546090";
+        private String name = "Светлана";
+        private int page_number = 0;
+        private String[] URLs = new String[5];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            URLs[0] = "http://eco-gribs.ru/";
+            URLs[1] = "http://lexopol-low.dostavka2.me/";
+            URLs[2] = "http://flk.officialhealth.ru/";
+            URLs[3] = "http://c.potencialex.ru/149/v1/";
+            URLs[4] = "http://titan-gel.com";
+        }
+
+        private void changePage(int page)
+        {
+            webBrowser1.Navigate(URLs[page]);        
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            try
+            {
+                switch (page_number)
+                {
+                    case 0:
+                        webBrowser1.Document.GetElementById("lv-formLanding1-phone").InnerText = number;
+
+                        foreach (HtmlElement he in webBrowser1.Document.GetElementsByTagName("button"))
+                            if (he.GetAttribute("name").Equals("au1th"))
+                                he.InvokeMember("click");
+                        //page_number++;
+                        //changePage(page_number);
+                        break;
+
+                    case 11:
+                        foreach (HtmlElement htmlElement in webBrowser1.Document.GetElementsByTagName("input"))
+                            if (htmlElement.GetAttribute("name").Equals("name"))
+                                htmlElement.InnerText = name;
+
+                        foreach (HtmlElement htmlElement in webBrowser1.Document.GetElementsByTagName("input"))
+                            if (htmlElement.GetAttribute("name").Equals("phone"))
+                                htmlElement.InnerText = number;
+
+                        foreach (HtmlElement htmlElement in webBrowser1.Document.GetElementsByTagName("button"))
+                            if (htmlElement.GetAttribute("type").Equals("subm1it"))
+                                htmlElement.InvokeMember("click");
+
+                        page_number++;
+                        changePage(page_number);
+                        break;
+                    case 22:
+                        foreach (HtmlElement htmlElement in webBrowser1.Document.GetElementsByTagName("input"))
+                            if (htmlElement.GetAttribute("name").Equals("name"))
+                                htmlElement.InnerText = name;
+
+                        foreach (HtmlElement htmlElement in webBrowser1.Document.GetElementsByTagName("input"))
+                            if (htmlElement.GetAttribute("name").Equals("phone"))
+                                htmlElement.InnerText = number;
+
+                        foreach (HtmlElement htmlElement in webBrowser1.Document.GetElementsByTagName("button"))
+                            if (htmlElement.GetAttribute("type").Equals("subm1it"))
+                                htmlElement.InvokeMember("click");
+                        page_number++;
+                        changePage(page_number);
+                        break;
+
+                    case 24:
+                        foreach (HtmlElement htmlElement in webBrowser1.Document.GetElementsByTagName("input"))
+                            if (htmlElement.GetAttribute("name").Equals("fio"))
+                                htmlElement.InnerText = name;
+
+                        foreach (HtmlElement htmlElement in webBrowser1.Document.GetElementsByTagName("input"))
+                            if (htmlElement.GetAttribute("name").Equals("phone"))
+                                htmlElement.InnerText = number;
+
+                        foreach (HtmlElement htmlElement in webBrowser1.Document.GetElementsByTagName("button"))
+                            if (htmlElement.GetAttribute("type").Equals("subm1it"))
+                                htmlElement.InvokeMember("click");
+                        break;
+                }
+                
+            }
+            catch (NullReferenceException)
+            {
+
+            }                
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            changePage(page_number);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
